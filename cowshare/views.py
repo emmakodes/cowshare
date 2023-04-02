64c5a1023278d3d658f7d8cb420a597b465d8578
emmakodes <emmamichael65@gmail.com>
2023-04-02 13:51:56 +0200

    added functions and html for index and products page

--- cowshare/views.py
+++ cowshare/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
+    
+    context = {
+        
+    }
+    return render(request, 'cowshare/index.html',context)
 
 def products(request):
-    pass
+    context = {
+        
+    }
+    return render(request, 'cowshare/products.html',context)
